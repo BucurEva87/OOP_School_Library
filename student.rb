e21c31723296128a25c5ec374dd1fa36c8cbf3ce
BucurEva87 <bucur.eva87@gmail.com>
2022-11-26 19:09:38 +0200

    Add Book, Rental, Classroom classes and set the up between them

--- student.rb
+++ student.rb
@@ -1,12 +1,19 @@
 require './person'
 
 class Student < Person
-  def initialize(classroom)
-    super(age, name, parent_permission)
+  attr_reader :classroom
+
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     @classroom = classroom
+    super(age, name, parent_permission: parent_permission)
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
-    '¯\(ツ)/¯'
+    '¯(ツ)/¯'
   end
 end
